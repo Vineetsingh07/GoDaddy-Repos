e54c6958119d5cc3821b448b44c18d97c8cb9bcc
Vineetsingh07 <vineetsingh2603@gmail.com>
2024-11-17 21:04:18 +0530

    Test Cases Config.

--- src/services/ApiCalls.js
+++ src/services/ApiCalls.js
@@ -1,15 +1,13 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_APP_NODE_JS_BASE_URL;
-
 class APICalls {
   async getRepoList() {
-    const url = `${BASE_URL}/orgs/godaddy/repos`;
+    const url = `https://api.github.com/orgs/godaddy/repos`;
     return await axios.get(url);
   }
 
   async getRepoDetails(repoName) {
-    const url = `${BASE_URL}/repos/godaddy/${repoName}`;
+    const url = `https://api.github.com/repos/godaddy/${repoName}`;
     return await axios.get(url);
   }
 }
