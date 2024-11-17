85c508101ae238fb90a4c02207c3a0683be82b3b
Vineetsingh07 <vineetsingh2603@gmail.com>
2024-11-17 23:08:59 +0530

    Write Test Cases

---
+++ babel.config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  presets: [
+    "@babel/preset-env", // For ES module support
+    "@babel/preset-react", // For JSX support
+  ],
+};
--- jest.config.cjs
+++ jest.config.cjs
@@ -1,3 +1,9 @@
 module.exports = {
+  transform: {
+    "^.+\\.[jt]sx?$": "babel-jest", // Apply Babel transformation to .jsx, .ts, .tsx files
+  },
+  transformIgnorePatterns: [
+    "/node_modules/(?!axios)/", // This allows Jest to transform axios and other packages that need it
+  ],
   testEnvironment: "jsdom",
 };
