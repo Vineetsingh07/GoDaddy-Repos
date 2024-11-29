a2355a5431a0f23a79a46ee56089afe495bc9f57
Vineetsingh07 <vineetsingh2603@gmail.com>
2024-11-30 01:02:50 +0530

    Code Refactor.

--- src/components/RepoList/RepoListItem.jsx
+++ src/components/RepoList/RepoListItem.jsx
@@ -3,7 +3,7 @@ import RepoCard from "./RepoCard";
 
 function RepoListItem({ repos }) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mb-14">
       {repos.map((repo) => (
         <RepoCard key={repo.id} repo={repo} />
       ))}
