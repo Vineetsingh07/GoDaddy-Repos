e38ec59bd4c50ed923876a86b7033f9217019b12
Vineetsingh07 <vineetsingh2603@gmail.com>
2024-12-02 00:57:30 +0530

    Code Refactor.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 import RepoList from "./components/RepoList/RepoList";
 import RepoDetails from "./components/RepoDetails/RepoDetails";
 import "./App.css";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<RepoList />} />
+      <Route path="/repo/:repoName" element={<RepoDetails />} />
+    </>
+  ),
+  {
+    future: {
+      v7_startTransition: true, // Opt into state update transition behavior
+      v7_relativeSplatPath: true, // Opt into new relative splat path behavior
+      v7_fetcherPersist: true, // Opt into new fetcher persistence behavior
+      v7_normalizeFormMethod: true, // Opt into uppercase form method normalization
+      v7_partialHydration: true, // Opt into partial hydration behavior
+      v7_skipActionErrorRevalidation: true, // Opt into error revalidation behavior
+      v7_startTransition: true,
+    },
+  }
+);
+
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<RepoList />} />
-        <Route path="/repo/:repoName" element={<RepoDetails />} />
-      </Routes>
-    </Router>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
